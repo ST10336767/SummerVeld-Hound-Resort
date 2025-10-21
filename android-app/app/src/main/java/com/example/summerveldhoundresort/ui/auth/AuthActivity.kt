fe695292d787d3f7465817dd63bfae83554b3f7f
mattesmi <127109845+st10314794@users.noreply.github.com>
2025-10-21 20:05:43 +0200

    apply saved light/dark themes from the login/signup screen of app

--- android-app/app/src/main/java/com/example/summerveldhoundresort/ui/auth/AuthActivity.kt
+++ android-app/app/src/main/java/com/example/summerveldhoundresort/ui/auth/AuthActivity.kt
@@ -7,14 +7,23 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.summerveldhoundresort.R
 import com.example.summerveldhoundresort.databinding.ActivityAuthBinding
+import com.example.summerveldhoundresort.utils.ThemeManager
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //added -> apply saved themes straight from login/signup of app
+        // Apply saved theme on fragment creation
+        val savedTheme = ThemeManager.getThemeMode(this)
+        ThemeManager.applyTheme(savedTheme)
+
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
     }
 }
